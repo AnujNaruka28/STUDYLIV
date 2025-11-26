a355e2228cd571784543ab75b0261fbb4282e326
AnujNaruka28 <143706882+AnujNaruka28@users.noreply.github.com>
2025-11-26 17:05:14 +0530

    Delete StudyLiv/frontend/.next directory

--- StudyLiv/frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"k+f5EdYyBkfmbTPCRPDG+qHui/kI7ybaow8aNUsRSnE=\"\n}"
--- StudyLiv/frontend/.next/static/chunks/app_course_[id]_take_page_tsx_a67ed533._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_components_cores_SingleCoursePage_TakeClient_tsx_9ae3468c._.js"
-],
-    source: "dynamic"
-});
